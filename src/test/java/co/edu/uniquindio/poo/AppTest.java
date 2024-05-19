6b15d68d1c8f9c9a76ac0b089ae83d63648922e2
Andres <andres.vanegas.0193@gmail.com>
2024-05-19 08:41:19 -0500

    complete data test

--- src/test/java/co/edu/uniquindio/poo/AppTest.java
+++ src/test/java/co/edu/uniquindio/poo/AppTest.java
@@ -9,6 +9,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
 
+import co.edu.uniquindio.poo.Vehiculo.Carro.CarroDao;
+import co.edu.uniquindio.poo.Vehiculo.Moto.MotoDao;
+import co.edu.uniquindio.poo.Vehiculo.Moto.TipoMoto;
+
 /**
  * Clase base de pruebas unitarias 
  */
@@ -24,4 +28,51 @@ public class AppTest {
         LOG.info("Finalizando test shouldAnswerWithTrue");
     }
 
+      //Test de datos completos para Moto clasica
+
+      @Test
+      public void testMotoClasica (){
+          LOG.info("Iniciando testMotoClasica");
+          MotoDao moto = new MotoDao("Andres", "HGD658", "2020", 250, TipoMoto.CLASICA);
+  
+          assertEquals("Andres", moto.getNombre());
+          assertEquals("HGD658", moto.getPlaca());
+          assertEquals("2020", moto.getModelo());
+          assertEquals(250, moto.getVelocidadMaxima());
+          assertEquals(TipoMoto.CLASICA, moto.getTipoMoto());
+          LOG.info("Finalizando testMotoClasica");
+  
+      }
+  
+  
+      //Test de datos completos para Moto hibrida
+  
+      @Test
+      public void testMotoHibrida (){
+          LOG.info("Iniciando testMotoHibrida");
+          MotoDao moto = new MotoDao("carol", "KFJ565", "2012", 300, TipoMoto.HIBRIDA);
+  
+          assertEquals("carol", moto.getNombre());
+          assertEquals("KFJ565", moto.getPlaca());
+          assertEquals("2012", moto.getModelo());
+          assertEquals(300, moto.getVelocidadMaxima());
+          assertEquals(TipoMoto.HIBRIDA, moto.getTipoMoto());
+          LOG.info("Finalizando testMotoHibrida");
+  
+      }
+  
+  
+      //Test de datos completos para carro
+  
+      @Test
+      public void testCarro (){
+          LOG.info("Iniciando testCarro");
+          CarroDao moto = new CarroDao("juan", "JDH985", "2000");
+  
+          assertEquals("juan", moto.getNombre());
+          assertEquals("JDH985", moto.getPlaca());
+          assertEquals("2000", moto.getModelo());
+          LOG.info("Finalizando testCarro");
+  
+      }
 }
