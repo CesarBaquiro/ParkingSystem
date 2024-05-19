1826771a30e29167cd3b2164caf984c9c2676986
Andres <andres.vanegas.0193@gmail.com>
2024-05-19 08:52:15 -0500

    add new tests

--- src/test/java/co/edu/uniquindio/poo/AppTest.java
+++ src/test/java/co/edu/uniquindio/poo/AppTest.java
@@ -4,11 +4,15 @@
 package co.edu.uniquindio.poo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
 
+import co.edu.uniquindio.poo.Espacio.EspacioDao;
+import co.edu.uniquindio.poo.Parqueadero.ParqueaderoDao;
 import co.edu.uniquindio.poo.Vehiculo.Carro.CarroDao;
 import co.edu.uniquindio.poo.Vehiculo.Moto.MotoDao;
 import co.edu.uniquindio.poo.Vehiculo.Moto.TipoMoto;
@@ -62,17 +66,41 @@ public class AppTest {
       }
   
   
-      //Test de datos completos para carro
-  
-      @Test
-      public void testCarro (){
-          LOG.info("Iniciando testCarro");
-          CarroDao moto = new CarroDao("juan", "JDH985", "2000");
-  
-          assertEquals("juan", moto.getNombre());
-          assertEquals("JDH985", moto.getPlaca());
-          assertEquals("2000", moto.getModelo());
-          LOG.info("Finalizando testCarro");
-  
-      }
+        //Test de datos completos para carro
+    
+        @Test
+        public void testCarro (){
+            LOG.info("Iniciando testCarro");
+            CarroDao moto = new CarroDao("juan", "JDH985", "2000");
+    
+            assertEquals("juan", moto.getNombre());
+            assertEquals("JDH985", moto.getPlaca());
+            assertEquals("2000", moto.getModelo());
+            LOG.info("Finalizando testCarro");
+        }
+
+        //Test para crear un espacio vacio correctamente
+        @Test
+        void testCrearEspacioVacio() {
+            LOG.info("Iniciando testCrearEspacioVacio");
+            EspacioDao espacio = new EspacioDao("1-1", true, false);
+
+            assertEquals("1-1", espacio.getId());
+            assertTrue(espacio.getEspacioHabilitado());
+            assertFalse(espacio.getOcupado());
+            LOG.info("Finalizando testCrearEspacioVacio");
+        }
+
+        //Test para obtener el espacio 
+        @Test
+        void testGetEspacio() {
+            LOG.info("Iniciando testGetEspacio");
+            EspacioDao[][] espacios = ParqueaderoDao.configuracionDefecto(3, 3);
+            EspacioDao espacio = ParqueaderoDao.getEspacio(0, 0);
+
+            assertNotNull(espacio); //Verifica que "espacio", no sea nulo
+            assertEquals("A-1", espacio.getId());
+            LOG.info("Finalizando testGetEspacio");
+        }
+
 }
